afd19fba29a648b6be969c7e16550d19d991c4d1
Shadlance <shadlance@gmail.com>
2015-07-16 12:46:36 +0700

    Add USERS-dbCore

---
+++ data/index.js
@@ -0,0 +1,12 @@
+var mongoose = require('../libs/mongoose'),
+    async = require('async'),
+    User = require('./models/user').User,
+    funcList = {
+        connectOpen: function(callback) { mongoose.connection.on('open', callback); },
+        createUser: function(callback) { User({ username: 'test1', password: 'qwerty' }).save(callback); }
+    };
+
+async.series(funcList, function() {
+    console.log(arguments);
+    mongoose.disconnect();
+});
